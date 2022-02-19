ee040d3efe40bffef4cd3f8fdeb8804227b2d680
matthew196 <matthew@thinkninjas.co.za>
2022-02-19 12:31:09 +0200

    init commit

---
+++ pages/index.tsx
@@ -0,0 +1,57 @@
+/* eslint-disable @next/next/no-img-element */
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.scss";
+import { IAdvice } from "../types/IAdvice";
+
+export default function Home() {
+  const [advice, setAdvice] = useState<IAdvice>({});
+
+  const getNewAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice");
+
+    const data = await res.json();
+
+
+    setAdvice(data.slip);
+  };
+
+  useEffect(() => {
+    async function _() {
+      await getNewAdvice();
+    }
+
+    _();
+  }, []);
+
+  const handleButtonClick = async () => {
+    await getNewAdvice();
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Random Advice</title>
+        <meta name="description" content="Best random advice app ever" />
+        <link rel="icon" href="/iamges/favicon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.card}>
+          <h6 className={styles.card__title}>Advice #{advice.id}</h6>
+          <q className={styles.card__quote}>{advice.advice}</q>
+          <div className={styles.card__divider}></div>
+          <div className={styles.card__row_centered}>
+            <div className={styles.button} onClick={handleButtonClick}>
+              <img
+                className={styles.button__icon}
+                src="/images/icon-dice.svg"
+                alt="Get random device"
+              />
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
